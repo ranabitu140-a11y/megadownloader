b3128e169b5f981b671a6cf00484a2a92d40d6d7
ranabitu140-a11y <ranabitu140@gmail.com>
2026-03-22 14:48:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,2 @@
 #!/bin/bash
-
-echo "Starting Cloudflare WARP..."
-# Start the WARP background service manually
-warp-svc &
-# Give it 5 seconds to boot up
-sleep 5
-
-# Register, set proxy mode, and connect
-warp-cli --accept-tos registration new
-warp-cli --accept-tos mode proxy
-warp-cli --accept-tos connect
-
-# Wait 2 seconds for the connection to establish
-sleep 2
-
-echo "WARP Connected! Starting Telegram Bot..."
 python bot.py
